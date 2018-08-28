8f06ecd040734fcf55906ed69cc0d2abd5eda7a1
Tarunkumar <rtarunk@apac.corpdir.net>
2018-08-28 09:53:02 +0530

    First

---
+++ spec/readfile-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ReadfileView from '../lib/readfile-view';
+
+describe('ReadfileView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
